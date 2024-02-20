6539744a61018273c7e93fd9cabb35e193962e8c
Vigneshwar Kandhaiya <vigneshwar.k2@gmail.com>
2024-02-20 22:49:30 +0530

    changed the stage03 and removed unexessary codes

--- stage03.py
+++ stage03.py
@@ -2,4 +2,4 @@ with open("artifacts01.txt","r") as f:
     text=f.read()
 
 with open("artifacts02.txt","w") as f:
-    text=f.write(text + "added a new stage")
+    f.write(text + "added a new stage")
